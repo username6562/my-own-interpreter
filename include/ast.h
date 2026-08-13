5dab2bedb107a5fe26a3662d42a4f7eefc50391d
username6562 <owanate656@gmail.com>
2026-08-13 19:37:54 +0100

    added if statement type

--- include/ast.h
+++ include/ast.h
@@ -17,11 +17,12 @@ typedef enum {
         GT_OR_EQUAL_TO, // Greater Than Or Equal To
         LESS_THAN_OP,
         GREATER_THAN_OP,
-        EQUALS_TO_OP
+        EQUALS_TO_OP,
 } ExprType;
 
 typedef struct Expr Expr;
-
+typedef struct Stmt Stmt;
+typedef struct StmtList StmtList;
 struct Expr {
         char *value;
         ExprType type;
@@ -29,34 +30,41 @@ struct Expr {
         Expr *right;
 };
 
-typedef enum { VAR_DECL_STMT } StmtType;
+typedef enum {
+        VAR_DECL_STMT,
+        VAR_REASSIGN_STMT,
+        IF_STMT
+} StmtType;
 
-typedef struct {
+struct Stmt {
         StmtType type;
         union {
                 struct {
                         char *type;
                         char *var_name;
                         Expr *value;
-                        bool is_declared;
                 } variable_decl;
+                struct {
+                        Expr *condition;
+                        StmtList *stmts;
+                        Stmt *elif_stmt;
+                } if_stmt;
         };
+};
 
-} Stmt;
-
-typedef struct {
+struct StmtList {
         Stmt **statements;
         int count;
-} StmtList;
+};
 
 Expr *create_int_literal(char *value);
 Expr *create_identifier_literal(char *value);
 Expr *create_bool_literal(char *value);
 Expr *create_string_literal(char *value);
-Expr *create_binary_expr(char *operator, Expr * left, Expr *right,
-                         ExprType type);
-Stmt *create_variable_decl_stmt(char *type, char *var_name, Expr *value,
-                                bool is_declared);
+Expr *create_binary_expr(char *operator, Expr * left, Expr *right, ExprType type);
+Stmt *create_variable_decl_stmt(char *type, char *var_name, Expr *value);
+Stmt *create_var_assignment_stmt(char *var_name, Expr *value);
+Stmt *create_if_stmt(Expr *conditon);
 StmtList *create_stmt_list();
 void print_stmt_list(StmtList *list);
 void print_expr(Expr *expr);
